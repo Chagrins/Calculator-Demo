5d19389f58e33c6c388140ed8095c6fe34780f32
Malcolm Gruber <malcolm.gruber@pixovr.com>
2025-08-12 10:16:01 -0400

    Pushing up code.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator;
+
+public class Calculator
+{
+    public static int Add(int x, int y) => x + y;
+    public static int Subtract(int x, int y) => x - y;
+    public static int Multiply(int x, int y) => x * y;
+    public static int Divide(int x, int y) => x / y;
+    public static int Mod(int x, int y) => x % y;
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿namespace UnitTest;
+using NUnit.Framework;
+using Calculator;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void AdditionTest()
+    {
+        Assert.That(5 == Calculator.Add(2, 3), Is.True);
+    }
+    
+    [Test]
+    public void SubtractionTest()
+    {
+        Assert.That(2 == Calculator.Subtract(5, 3), Is.True);
+    }
+    
+    [Test]
+    public void MultiplyTest()
+    {
+        Assert.That(360 == Calculator.Multiply(120, 3), Is.True);
+    }
+    
+    [Test]
+    public void DivideTest()
+    {
+        Assert.That(100 == Calculator.Divide(500, 5), Is.True);
+    }
+    
+    [Test]
+    public void DivideByZeroTest()
+    {
+        Assert.Throws<DivideByZeroException>(() => Calculator.Divide(99, 0));
+    }
+}
